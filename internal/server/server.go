7f835af5632f014533b91de9dcf3e2e2ac0e4fb2
Lukas Jarosch <lukas.jarosch@mail.com>
2019-03-26 20:22:57 +0100

    move handler implementation to handler.go
    Signed-off-by: Lukas Jarosch <lukas.jarosch@mail.com>

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,8 +1,6 @@
 package server
 
 import (
-	"context"
-
 	"github.com/lukasjarosch/microservice-structure-protobuf/greeter"
 	"github.com/lukasjarosch/microservice-structure/internal/config"
 	"github.com/lukasjarosch/microservice-structure/internal/service"
@@ -55,34 +53,4 @@ func NewExampleServiceServer(config *config.Config, logger *logrus.Logger) *exam
 		GRPC:        server,
 		HTTPGateway: gatewayServer,
 	}
-}
-
-// exampleServiceHandler is the transport-layer wrapper of our business-logic in the server package
-// Everything concerning requests/responses belongs in here. Only conversion (business-model <-> protobuf) should happen here actually.
-type exampleServiceHandler struct {
-	implementation *service.ExampleService
-}
-
-func NewExampleServiceHandler(implementation *service.ExampleService) *exampleServiceHandler {
-	return &exampleServiceHandler{
-		implementation: implementation,
-	}
-}
-
-func (e *exampleServiceHandler) Greeting(ctx context.Context, request *greeter.GreetingRequest) (*greeter.GreetingResponse, error) {
-	greeting, err := e.implementation.Greeting(request.Name)
-	if err != nil {
-		return nil, err
-	}
-
-	return &greeter.GreetingResponse{Greeting: greeting}, nil
-}
-
-func (e *exampleServiceHandler) Farewell(ctx context.Context, request *greeter.FarewellRequest) (*greeter.FarewellResponse, error) {
-	farewell, err := e.implementation.Farewell(request.Name)
-	if err != nil {
-		return nil, err
-	}
-
-	return &greeter.FarewellResponse{Farewell: farewell}, nil
 }
